5f1771c87870369317e6c83a2e1c0ddba909d176
Dilan Lopez <dilanlopez009@gmail.com>
2022-10-18 15:28:07 -0300

    all default routes

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express')
-
+const checklistRouter = require('./src/routes/checklist')
 const app = express()
 
 //middleware para utilização de json
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('<h1> lista de tarefas </h1>')
-})
-
-app.get('/json', (req, res) => {
-  res.json({
-    title: 'Tarefa X',
-    done: true
-  })
-})
+// utilização das rotas como se fosse middleware
+app.use('/checklists', checklistRouter)
 
 app.listen(3000, () => {
   console.log('server iniciado')
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -3,9 +3,33 @@ const express = require('express')
 // metódo router permite criar rota dos arquivos
 const router = express.Router()
 
-router.get('checklist', (req, res) => {
+// rota get padrão
+router.get('/', (req, res) => {
   console.log('rota get')
-  res.send()
+  res.send(req.body)
+})
+
+// rota post
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.status(200).send(req.body)
+})
+
+//rota get com param
+router.get('/:id', (req, res) => {
+  console.log(req.params.id)
+  res.send(`id: ${req.params.id}`)
+})
+
+//rota put
+router.put('/:id', (req, res) => {
+  console.log(req.params.id)
+  res.send(`put id: ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+  console.log(req.params.id)
+  res.send(`delete id: ${req.params.id}`)
 })
 
 module.exports = router
