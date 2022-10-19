398b672f08476f03d0fc0514a2ef45feb4295930
Dilan Lopez <dilanlopez009@gmail.com>
2022-10-19 11:17:55 -0300

    first page with ejs

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const path = require('path')
 const checklistRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
+
 require('./config/database')
 
 const app = express()
-
-//middleware para utilização de json
 app.use(express.json())
 
-// utilização das rotas como se fosse middleware
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
 app.use('/checklists', checklistRouter)
+app.use('/', rootRouter)
 
 app.listen(3000, () => {
   console.log('server iniciado')
