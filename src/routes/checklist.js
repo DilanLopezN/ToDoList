a5346050c29e6d1c85a0223a5641e696bccc7de3
Dilan Lopez <dilanlopez009@gmail.com>
2022-10-20 09:56:08 -0300

    add put edit

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -27,6 +27,17 @@ router.get('/new', async (req, res) => {
   }
 })
 
+router.get('/:id/edit', async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id)
+    res.status(200).render('checklists/edit', { checklist: checklist })
+  } catch (error) {
+    res.status(500).render('pages/error', {
+      error: 'Erro ao exibir edição lista de tarefas'
+    })
+  }
+})
+
 // rota post
 router.post('/', async (req, res) => {
   let { name } = req.body.checklist
@@ -56,16 +67,17 @@ router.get('/:id', async (req, res) => {
 
 //rota put
 router.put('/:id', async (req, res) => {
-  let { name } = req.body
+  let { name } = req.body.checklist
+  let checklist = await Checklist.findById(req.params.id)
+
   try {
-    let checklist = await Checklist.findByIdAndUpdate(
-      req.params.id,
-      { name },
-      { new: True }
-    )
-    res.status(200).json(checklist)
+    await checklist.update({ name })
+    res.redirect('/checklists')
   } catch (error) {
-    res.status(422).json(error)
+    let errors = error.errors
+    res
+      .status(422)
+      .render('checklists/edit', { checklist: { ...checklist, errors } })
   }
 })
 
